2d259b58a18ebfc537abb92101010c2479bff928
Justin Fagnani <justin@fagnani.com>
2025-11-24 22:21:53 -0800

    [compiler] Refactor code generation tests to use a compile helper function

--- packages/compiler/src/test/codegen_test.ts
+++ packages/compiler/src/test/codegen_test.ts
@@ -3,18 +3,21 @@ import {suite, test} from 'node:test';
 import {Parser} from '../lib/parser.js';
 import {CodeGenerator} from '../lib/codegen.js';
 
+async function compile(input: string) {
+  const parser = new Parser(input);
+  const ast = parser.parse();
+  const codegen = new CodeGenerator(ast);
+  const bytes = codegen.generate();
+  const result = await WebAssembly.instantiate(bytes.buffer as ArrayBuffer);
+  return result.instance.exports;
+}
+
 suite('CodeGenerator', () => {
   test('should compile and run a simple add function', async () => {
     const input = 'export let add = (a: i32, b: i32) => a + b;';
-    const parser = new Parser(input);
-    const ast = parser.parse();
-
-    const codegen = new CodeGenerator(ast);
-    const bytes = codegen.generate();
-
-    const result = await WebAssembly.instantiate(bytes);
-    // @ts-ignore
-    const add = result.instance.exports.add as (a: number, b: number) => number;
+    const {add} = (await compile(input)) as {
+      add: (a: number, b: number) => number;
+    };
 
     assert.strictEqual(add(10, 20), 30);
     assert.strictEqual(add(-5, 5), 0);
@@ -22,33 +25,18 @@ suite('CodeGenerator', () => {
 
   test('should compile and run a nested math expression', async () => {
     const input = 'export let calc = (a: i32, b: i32) => (a + b) * 2;';
-    const parser = new Parser(input);
-    const ast = parser.parse();
-
-    const codegen = new CodeGenerator(ast);
-    const bytes = codegen.generate();
-
-    const result = await WebAssembly.instantiate(bytes);
-    // @ts-ignore
-    const calc = result.instance.exports.calc as (
-      a: number,
-      b: number,
-    ) => number;
+    const {calc} = (await compile(input)) as {
+      calc: (a: number, b: number) => number;
+    };
 
     assert.strictEqual(calc(2, 3), 10); // (2 + 3) * 2 = 10
   });
 
   test('should compile and run a function with block body and return', async () => {
     const input = 'export let add = (a: i32, b: i32) => { return a + b; };';
-    const parser = new Parser(input);
-    const ast = parser.parse();
-
-    const codegen = new CodeGenerator(ast);
-    const bytes = codegen.generate();
-
-    const result = await WebAssembly.instantiate(bytes);
-    // @ts-ignore
-    const add = result.instance.exports.add as (a: number, b: number) => number;
+    const {add} = (await compile(input)) as {
+      add: (a: number, b: number) => number;
+    };
 
     assert.strictEqual(add(10, 20), 30);
   });
@@ -56,15 +44,7 @@ suite('CodeGenerator', () => {
   test('should compile and run a function with local variables', async () => {
     const input =
       'export let addOne = (a: i32) => { let x = 1; return a + x; };';
-    const parser = new Parser(input);
-    const ast = parser.parse();
-
-    const codegen = new CodeGenerator(ast);
-    const bytes = codegen.generate();
-
-    const result = await WebAssembly.instantiate(bytes);
-    // @ts-ignore
-    const addOne = result.instance.exports.addOne as (a: number) => number;
+    const {addOne} = (await compile(input)) as {addOne: (a: number) => number};
 
     assert.strictEqual(addOne(10), 11);
   });
@@ -80,15 +60,7 @@ suite('CodeGenerator', () => {
         return 0;
       };
     `;
-    const parser = new Parser(input);
-    const ast = parser.parse();
-
-    const codegen = new CodeGenerator(ast);
-    const bytes = codegen.generate();
-
-    const result = await WebAssembly.instantiate(bytes);
-    // @ts-ignore
-    const check = result.instance.exports.check as (a: number) => number;
+    const {check} = (await compile(input)) as {check: (a: number) => number};
 
     assert.strictEqual(check(11), 1);
     assert.strictEqual(check(10), 0);
@@ -104,15 +76,7 @@ suite('CodeGenerator', () => {
         return a;
       };
     `;
-    const parser = new Parser(input);
-    const ast = parser.parse();
-
-    const codegen = new CodeGenerator(ast);
-    const bytes = codegen.generate();
-
-    const result = await WebAssembly.instantiate(bytes);
-    // @ts-ignore
-    const abs = result.instance.exports.abs as (a: number) => number;
+    const {abs} = (await compile(input)) as {abs: (a: number) => number};
 
     assert.strictEqual(abs(-10), 10);
     assert.strictEqual(abs(10), 10);
@@ -129,17 +93,10 @@ suite('CodeGenerator', () => {
         return false;
       };
     `;
-    const parser = new Parser(input);
-    const ast = parser.parse();
-
-    const codegen = new CodeGenerator(ast);
-    const bytes = codegen.generate();
-
-    const result = await WebAssembly.instantiate(bytes);
-    // @ts-ignore
-    const getTrue = result.instance.exports.getTrue as () => number;
-    // @ts-ignore
-    const getFalse = result.instance.exports.getFalse as () => number;
+    const {getTrue, getFalse} = (await compile(input)) as {
+      getTrue: () => number;
+      getFalse: () => number;
+    };
 
     assert.strictEqual(getTrue(), 1);
     assert.strictEqual(getFalse(), 0);
