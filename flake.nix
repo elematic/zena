dcf58efe96d6e208a01f237d3bcdde2e3e885398
Peter Burns <rictic@gmail.com>
2025-12-25 17:38:54 -0800

    Add a nix flake. (#34)
    `nix develop` to enter a dev shell with a known good version of nodejs (and any other deps added to `devShells.default`.
    
    Also provides a binary output named 'zena' that's the compiler's CLI.

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  description = "Zena - A statically typed language targeting WASM-GC";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        nodejs = pkgs.nodejs_24;
+
+        zena = pkgs.buildNpmPackage {
+          pname = "zena";
+          version = "0.0.1";
+
+          src = ./.;
+
+          npmDepsHash = "sha256-2bFhkmc2bAS/ITyf2WO4Za0gH229Do9+b+s8byqD+6c=";
+
+          nativeBuildInputs = [ nodejs ];
+
+          buildPhase = ''
+            runHook preBuild
+            npm run build
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/lib/zena
+            cp -r packages $out/lib/zena/
+            cp -r node_modules $out/lib/zena/
+            cp package.json $out/lib/zena/
+
+            mkdir -p $out/bin
+            cat > $out/bin/zena << EOF
+            #!${pkgs.bash}/bin/bash
+            exec ${nodejs}/bin/node $out/lib/zena/packages/cli/lib/cli.js "\$@"
+            EOF
+            chmod +x $out/bin/zena
+
+            runHook postInstall
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Zena programming language compiler";
+            homepage = "https://github.com/nicolo-ribaudo/zena-lang";
+            license = licenses.mit;
+            mainProgram = "zena";
+          };
+        };
+      in
+      {
+        packages = {
+          default = zena;
+          zena = zena;
+        };
+
+        apps = {
+          default = flake-utils.lib.mkApp {
+            drv = zena;
+            name = "zena";
+          };
+          zena = flake-utils.lib.mkApp {
+            drv = zena;
+            name = "zena";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            nodejs
+          ];
+
+          shellHook = ''
+            echo "Zena development environment"
+            echo "Node.js version: $(node --version)"
+            echo "npm version: $(npm --version)"
+            echo ""
+            echo "Run 'npm install' to install dependencies"
+            echo "Run 'npm run build' to build the compiler"
+            echo "Run 'npm test' to run tests"
+          '';
+        };
+      }
+    );
+}
