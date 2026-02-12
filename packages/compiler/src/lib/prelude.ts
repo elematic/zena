318b4d64623df73fc98f203e812bc094c3af268a
Justin Fagnani <justin@fagnani.com>
2026-02-12 15:52:56 -0800

    [stdlib] Add zena:option to the prelude

--- packages/compiler/src/lib/prelude.ts
+++ packages/compiler/src/lib/prelude.ts
@@ -1,6 +1,7 @@
 export const prelude = `
 import { String } from 'zena:string';
 import { Error, IndexOutOfBoundsError } from 'zena:error';
+import { Option, Some, None, some, none } from 'zena:option';
 import { Sequence, MutableSequence } from 'zena:sequence';
 import { BoundedRange, FromRange, ToRange, FullRange } from 'zena:range';
 import { ImmutableArray } from 'zena:immutable-array';
